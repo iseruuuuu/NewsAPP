2fea4b1bb57a61c0d61664eff932c4667435983e
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-01-05 16:09:16 +0900

    ニュースアプリを作成しました。

---
+++ AnimeNews/Controller/Page5ViewController.swift
@@ -0,0 +1,199 @@
+
+
+//
+//  Page1ViewController.swift
+//  AnimeNews
+//
+//  Created by 井関竜太郎 on 2021/01/05.
+//
+
+import UIKit
+import SegementSlide
+
+class Page5ViewController: UITableViewController, SegementSlideContentScrollViewDelegate, XMLParserDelegate{
+    
+    //XMLパーサーのインスタンスを作成
+    var parser = XMLParser()
+    
+    //RSSのパース内の現在の要素名
+    //  var currentElememt:String!
+    var currentElememtName:String!
+    
+    var newsItems = [NewsItems]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = .clear
+        
+        let image = UIImage(named: "4")
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.tableView.frame.size.width, height: self.tableView.frame.size.height))
+        
+        imageView.image = image
+        self.tableView.backgroundView = imageView
+        
+        //XMLパース
+        let urlString = "https://news.yahoo.co.jp/pickup/rss.xml"
+        let url:URL = URL(string: urlString)!
+        parser = XMLParser(contentsOf: url)!
+        parser.delegate = self
+        parser.parse()
+        
+    }
+    
+    @objc var scrollView: UIScrollView {
+        return tableView
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return newsItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.frame.size.height/5
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+        
+        cell.backgroundColor = .clear
+        
+        let newsItem = self.newsItems[indexPath.row]
+        
+        cell.textLabel?.text = newsItem.title
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 15.0)
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.numberOfLines = 2
+        
+        cell.detailTextLabel?.text = newsItem.description
+        cell.detailTextLabel?.textColor = .white
+        cell.detailTextLabel?.numberOfLines = 3
+        
+        
+        return cell
+    }
+    
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElememtName = nil
+        if elementName == "item" {
+            self.newsItems.append(NewsItems())
+            
+                //item
+            
+        }else{
+            currentElememtName = elementName
+        }
+    }
+    
+    //internal func parser(_ parser: XMLParser, foundCharacters string: String) {
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if self.newsItems.count > 0 {
+            
+            //配列の中身は、[1.2.3.4.5] 0番目を読み込むために「−１」をする。
+            let lastItem = self.newsItems[self.newsItems.count - 1]
+            
+            switch self.currentElememtName {
+            
+            case "title":
+                lastItem.title = string
+                
+            case "link":
+                lastItem.url = string
+                
+            case "pubDate":
+                lastItem.pubDate = string
+                
+            case "description":
+                lastItem.description = string
+            default:break
+            }
+        }
+    }
+    
+    
+    //<title>タイトル<title>
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        self.currentElememtName = nil
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        
+        self.tableView.reloadData()
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //WebViewControllerにurlを渡して表示させる。
+        
+        let webViewController = WebViewController()
+        webViewController.modalTransitionStyle = .crossDissolve
+        let newsItem = newsItems[indexPath.row]
+        UserDefaults.standard.set(newsItem.url, forKey: "url")
+        present(webViewController, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ AnimeNews/Modle/NewsItems.swift
@@ -0,0 +1,24 @@
+//
+//  NewsItems.swift
+//  AnimeNews
+//
+//  Created by 井関竜太郎 on 2021/01/05.
+//
+
+import Foundation
+
+class NewsItems {
+    
+    var title: String?
+    //url
+    var url: String?
+    var link: String?
+    //日にち
+    var pubDate: String?
+    //メッセージ
+    var description: String?
+    //
+    var comments: String?
+    
+
+}
